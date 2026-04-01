96ecca0147d7d6932bd77b9aec513fd8cda1f56d
Sujal Thapa <sujalthapa138@gmail.com>
2026-04-01 10:32:52 +0530

    sum

--- J1.java
+++ J1.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class J1{
+    public static void sum(int a, int b){
+        System.out.println("the sum of a and b is: "+(a+b));
+    }
     public static void main(String[] args) {
-        System.err.println("helloadd");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sum(a,b);
     }
 }
