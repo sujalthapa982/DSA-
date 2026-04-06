088293f9b7a483cb07ba37a0e068913357b491ee
Sujal Thapa <sujalthapa138@gmail.com>
2026-04-06 10:49:39 +0530

    array

--- J1.java
+++ J1.java
@@ -1,18 +1,25 @@
-
 class J1{
-    public static void countnum(int arr[],int a){
-        int count =0;
+    public static void main(String[] args) {
+        int arr [][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
         for(int i=0; i<arr.length; i++){
-            if(arr[i] == a){
-                count++;
+            for(int j=0; j<3; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int key = 3;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i][j]==key){
+                    System.out.println("element found at: i="+i+"and j="+j);
+                }
             }
         }
-        System.out.println(count);
     }
-    
-    public static void main(String[] args) {
-        int a = 3;
-        int arr[] = {1,3,3,3,3};
-        countnum(arr, a);
-    }
+
 }
+    
